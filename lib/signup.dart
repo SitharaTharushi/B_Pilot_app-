b85bf2650ff8dcf8f8ca5ba4e68f05ffa8087f20
Sithara Tharushi <itt1819030@tec.rjt.ac.lk>
2024-03-02 23:03:15 +0530

    Update signup.dart
    Design signup interface

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,3 @@
-// signup.dart
-
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatelessWidget {
@@ -9,41 +7,109 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'B.Pilot',
-          style: TextStyle(color: Colors.black),
+        title: RichText(
+          text: const TextSpan(
+            text: 'B.',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44,
+              fontWeight: FontWeight.bold,
+            ),
+            children: [
+              TextSpan(
+                text: 'Pilot',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 232, 229, 228),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
-        backgroundColor: Colors.yellow,
+        backgroundColor: const Color.fromARGB(255, 247, 224, 26),
+        centerTitle: true,
+        toolbarHeight: 150,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Sign Up as a ',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            const Row(
+              children: [
+                Text(
+                  'Sign Up as a ',
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup/passenger');
-                },
-                icon: const Icon(Icons.person),
-                label: const Text('Passenger'),
+            Center(
+              child: SizedBox(
+                width: 200,
+                child: OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup/passenger');
+                  },
+                  icon: const Column(
+                    children: [
+                      Icon(Icons.person, size: 120, color: Colors.black),
+                      SizedBox(height: 5),
+                      Text(
+                        'Passenger',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      )
+                    ],
+                  ),
+                  label: const Text(''),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    side: const BorderSide(color: Colors.black, width: 1),
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                  ),
+                ),
               ),
             ),
             const SizedBox(height: 20),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup/pilot');
-                },
-                icon: const Icon(Icons.directions_bus),
-                label: const Text('Pilot'),
+            Center(
+              child: SizedBox(
+                width: 200,
+                child: OutlinedButton.icon(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/signup/pilot');
+                  },
+                  icon: const Column(
+                    children: [
+                      Icon(Icons.directions_bus,
+                          size: 120, color: Colors.black),
+                      SizedBox(height: 5),
+                      Text(
+                        'Pilot',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      )
+                    ],
+                  ),
+                  label: const Text(''),
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    side: const BorderSide(color: Colors.black, width: 1),
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                  ),
+                ),
               ),
             ),
           ],
